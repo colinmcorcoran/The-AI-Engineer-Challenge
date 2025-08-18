6d2f68836c2a4372fd39dc2cf3fe4d8abb0f4e90
ccorcoran-nydig <colin.corcoran@nydig.com>
2025-08-18 16:52:37 -0400

    add vercel

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/next"
 
 const App: React.FC = () => {
   return (
     <div>
+      <Analytics/>
       <h1>Welcome to The AI Engineer Challenge Frontend</h1>
     </div>
   );
