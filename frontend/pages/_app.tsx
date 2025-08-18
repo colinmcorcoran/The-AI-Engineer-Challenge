13540b0a3805eb3aea212ca6bd5d87e3712e6b65
ccorcoran-nydig <colin.corcoran@nydig.com>
2025-08-18 16:56:47 -0400

    fix vercel?

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
