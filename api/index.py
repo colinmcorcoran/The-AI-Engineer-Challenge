75ed72097128f27652334b1835426b45201d07c4
ccorcoran-nydig <colin.corcoran@nydig.com>
2026-02-03 13:23:15 -0500

    fancy up my bot

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -5,6 +6,7 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import logging
+import yfinance as yf
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,21 +36,123 @@ def root():
     logger.info("Got root")
     return {"status": "ok"}
 
+
+def get_stock_details(ticker):
+    stock_data = yf.Ticker(ticker)
+    return stock_data.info
+
+
 @app.post("/api/chat")
-def chat(request: ChatRequest):
+def chat2(request: ChatRequest):
     logger.info(f"Got chat {request.__dict__}")
     if not os.getenv("OPENAI_API_KEY"):
         raise HTTPException(status_code=500, detail="OPENAI_API_KEY not configured")
     
     try:
         user_message = request.message
-        response = client.chat.completions.create(
-            model="gpt-5",
-            messages=[
+        input_list=[
                 {"role": "system", "content": "You are a supportive mental coach."},
                 {"role": "user", "content": user_message}
-            ]
+        ]
+
+        available_tools = {"get_stock_details": get_stock_details}
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_stock_details",
+                    "description": "gets up to date stock information for a given stock symbol",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "ticker": {
+                                "type": "string",
+                                "description": "stock symbol to get details for"
+                                }
+                        },
+                        "required": ["ticker"]
+                    },
+                }
+            },
+        ]
+        model = "gpt-5"
+        
+        response = client.chat.completions.create(
+            model=model,
+            messages=input_list,
+            tools=tools
         )
-        return {"reply": response.choices[0].message.content}
+        if response.choices[0].message.tool_calls is None:
+            return {"reply": response.choices[0].message.content}
+
+
+        tool_calls_stream = client.chat.completions.create(
+            messages=input_list,
+            model=model,
+            tools=tools,
+            stream=True
+        )
+
+        chunks = []
+        for chunk in tool_calls_stream:
+            if len(chunk.choices) > 0:
+                chunks.append(chunk)
+
+        arguments = []
+        tool_call_idx = -1
+        for chunk in chunks:
+
+            if chunk.choices[0].delta.tool_calls:
+                tool_call = chunk.choices[0].delta.tool_calls[0]
+
+                if tool_call.index != tool_call_idx:
+                    if tool_call_idx >= 0:
+                        print(
+                            f"streamed tool call arguments: {arguments[tool_call_idx]}"
+                        )
+                    tool_call_idx = chunk.choices[0].delta.tool_calls[0].index
+                    arguments.append("")
+                if tool_call.id:
+                    print(f"streamed tool call id: {tool_call.id} ")
+
+                if tool_call.function:
+                    if tool_call.function.name:
+                        print(f"streamed tool call name: {tool_call.function.name}")
+
+                    if tool_call.function.arguments:
+                        arguments[tool_call_idx] += tool_call.function.arguments
+
+        if len(arguments):
+            print(f"streamed tool call arguments: {arguments[-1]}")
+
+        print("\n\n")
+
+        input_list.append({
+            "role": "assistant",
+            "tool_calls": response.choices[0].message.tool_calls
+        })
+
+        completion_tool_calls = response.choices[0].message.tool_calls
+        for call in completion_tool_calls:
+            tool_to_call = available_tools[call.function.name]
+            args = json.loads(call.function.arguments)
+            result = tool_to_call(**args)
+            print(result)
+            input_list.append({
+                "role": "tool",
+                "content": json.dumps(result),
+                "tool_call_id": call.id,
+                "name": call.function.name
+            })
+        
+        response_2 = client.chat.completions.create(messages=input_list,
+                                                        model=model,
+                                                        tools=tools,
+                                                        stream=False)
+        
+        
+        print(response_2)
+        return {"reply": response_2.choices[0].message.content}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error calling OpenAI API: {str(e)}")
+
